d3099ccb8f64ab73bf53486665418374c03a29b3
mhlee <mihyun0921@gmail.com>
2022-09-26 21:39:07 +0900

    [S07P22D210-425] feature - http 설정 추가 & cors

--- Back/alphagom/src/main/java/com/d210/alphagom/config/SecurityConfig.java
+++ Back/alphagom/src/main/java/com/d210/alphagom/config/SecurityConfig.java
@@ -9,7 +9,12 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor //final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 롬복 어노테이션
 @EnableWebSecurity // Spring Security 설정 활성화
@@ -21,23 +26,54 @@ public class SecurityConfig{
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors()
+                .configurationSource(corsConfigurationSource())
+            .and()
+                .sessionManagement()
+                // 웹 통신 방식 -> stateless 설정 - jwt 때문에 가능
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .csrf().disable() // csrf 보안 토큰 disable처리.
                 .headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션 disable
-                .and()
+            .and()
                 .authorizeRequests()// URL별 권한 권리
                 .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
                 .antMatchers("/api/v1/test/").permitAll()
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // /api/v1/** 은 USER권한만 접근 가능
+//                    .antMatchers("/api/be/**").permitAll()
+                .antMatchers("/api/be/**").hasRole(Role.USER.name()) // /api/v1/** 은 USER권한만 접근 가능
                 .anyRequest().authenticated() // anyRequest : 설정된 값들 이외 나머지 URL 나타냄, authenticated : 인증된 사용자
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint() // oauth2 로그인 성공 후 가져올 때의 설정들
+            .and()
+                .oauth2Login() // OAuth2Login 설정 시작
+                .userInfoEndpoint() // oauth2Login 성공 이후의 설정을 시작
                 // 소셜로그인 성공 시 후속 조치를 진행할 UserService 인터페이스 구현체 등록
                 .userService(customOAuth2UserService); // 리소스 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능 명시
 
+        // 지정된 필터 앞에 커스텀 필터를 추가
+        // 인증을 처리하는 기본필터 UsernamePasswordAuthenticationFilter 전에 필터를 실행하겠다.
+//        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    /*
+     * 토큰 필터 설정
+     * */
+//    @Bean
+//    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+//        return new TokenAuthenticationFilter(tokenProvider);
+//    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
