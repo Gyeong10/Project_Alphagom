119aaacbb8f3bf0d4cdb39690349da972d627e33
GyeongYeol <hrq789@naver.com>
2022-09-26 23:30:01 +0900

    [S07P22D210-227] feature - RefreshToken 엔티티 및 레포지토리 생성

---
+++ Back/alphagom/src/main/java/com/d210/alphagom/domain/repository/jwt/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.d210.alphagom.domain.repository.jwt;
+
+import com.d210.alphagom.domain.entity.jwt.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}
