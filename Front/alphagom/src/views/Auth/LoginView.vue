994c819bb57246f9a054184b2e072fb6dd0ddc58
mhlee <mihyun0921@gmail.com>
2022-10-04 01:01:00 +0900

    feature - 로그인페이지 구현

--- Front/alphagom/src/views/Auth/LoginView.vue
+++ Front/alphagom/src/views/Auth/LoginView.vue
@@ -2,26 +2,56 @@
   <div class="container">
     <div class="container-bg"></div>
     <div class="userInfo">
-      <p>로그인</p>
-      {{ username }}
+      <ul>
+        <li>
+          <a :href="socialLoginUrl('kakao')">
+            <!-- <div class="icon-box">
+              <img src="/social/kakao-icon.svg" />
+            </div> -->
+            <p>카카오톡 계정으로 로그인</p>
+          </a>
+        </li>
+        <li>
+          <a :href="socialLoginUrl('naver')">
+            <!-- <div class="icon-box">
+              <img src="/social/naver-icon.png" />
+            </div> -->
+            <p>네이버 계정으로 로그인</p>
+          </a>
+        </li>
+        <li>
+          <a :href="socialLoginUrl('google')">
+            <!-- <div class="icon-box">
+              <img src="/social/google-icon.svg" />
+            </div> -->
+            <p>구글 계정으로 로그인</p>
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { useBgStore } from "@/stores/bg"
-  import { computed, onMounted } from 'vue'
-  
-  // 배경 경로 수정
-  onMounted (() => {
-    // store의 bgUrlState 값을 직접 변경
-    bgStore.bgUrlState = 'url("/map_bg_picture_filter_low.png")'
-    console.log(bgStore.bgUrlState.value)
-  })
-  const bgStore = useBgStore()
-  // state 감시자
-  const bgwatching = computed(() => bgStore.bgUrlState)
-  </script>
+import { useBgStore } from "@/stores/bg";
+import { computed, onMounted } from "vue";
+import $ from "@/utils";
+
+// 배경 경로 수정
+onMounted(() => {
+  // store의 bgUrlState 값을 직접 변경
+  bgStore.bgUrlState = 'url("/map_bg_picture_filter_low.png")';
+  console.log(bgStore.bgUrlState.value);
+});
+const bgStore = useBgStore();
+// state 감시자
+const bgwatching = computed(() => bgStore.bgUrlState);
+
+const socialLoginUrl = (socialType) => {
+  console.log(socialType);
+  return $.getSocialLoginUrl(socialType);
+};
+</script>
 
 <style scoped>
 .container-bg {
@@ -32,10 +62,8 @@
   left: 0px;
   width: 926px;
   height: 428px;
-} 
+}
 .container {
-  /* background-image: url("src/assets/image/홈_배경_low.png"); */
-  background-color: blueviolet;
   width: 100vw;
   height: 100vh;
 }
---
+++ Front/alphagom/src/views/Auth/OAuthRedirectView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+import { useRoute, useRouter } from "vue-router";
+
+const store = useStore();
+const route = useRoute();
+const router = useRouter();
+const token = route.query.token;
+
+if (token) {
+  store.dispatch("auth/saveToken", token);
+  store.dispatch("auth/fetchUser");
+}
+router.replace("/board");
+</script>
