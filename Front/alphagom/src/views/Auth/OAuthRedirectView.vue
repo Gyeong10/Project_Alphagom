065eb872258faa5066fc9e799b8cf593e0b0dc80
박경열 <hrq789@naver.com>
2022-10-05 17:06:27 +0900

    feature - kakao 로그인 리다이렉트 화면 작성

--- Front/alphagom/src/views/Auth/OAuthRedirectView.vue
+++ Front/alphagom/src/views/Auth/OAuthRedirectView.vue
@@ -1,19 +1,77 @@
 <template>
-  <div></div>
+  <div>
+    <div class="userInfo">
+      <h3>이름</h3>
+      <input 
+        type="text"
+        v-model="form.name"
+        required
+        >
+      <h3>이메일</h3>
+      <input 
+        type="text"
+        v-text="form.email"
+        v-model="form.email"
+        required
+        disabled
+        >
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { useStore } from "vuex";
+import { useAuthStore } from "@/stores/auth";
 import { useRoute, useRouter } from "vue-router";
+import { ref } from "vue";
 
-const store = useStore();
+
+import axios from "axios";
+
+const store = useAuthStore();
 const route = useRoute();
 const router = useRouter();
-const token = route.query.token;
 
-if (token) {
-  store.dispatch("auth/saveToken", token);
-  store.dispatch("auth/fetchUser");
+const codes = route.query.code;
+const form = ref({
+  password: "",
+  email: "",
+  name: "",
+});
+
+function getToken() {
+  console.log("code : " + codes)
+  axios
+    .get("http://localhost:8080/api/be/klogin?authorize_code=" + codes)
+    .then((res) => {
+      console.log(res.data);
+      form.value.email = res.data.email;
+      form.value.password = res.data.id;
+      if (form.value.password == undefined) {
+        alert("잘못된 접근");
+        router.replace("/");
+      } else {
+        alert("성공");
+      }
+    })
 }
-router.replace("/board");
+
+if (codes) {
+  getToken(codes);
+}
+
+
+// if (token) {
+//   store.saveToken(token);
+//   store.fetchUserInfo();
+// }
+// router.replace("/");
 </script>
+
+<style scoped>
+.userInfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: black;
+}
+</style>
